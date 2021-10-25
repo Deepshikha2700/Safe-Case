de070e614712724fcb40b302c57398f600599a61
Deepshikha2700 <89649896+Deepshikha2700@users.noreply.github.com>
2021-10-25 20:16:27 +0530

    Update settings.py

--- SafePassword/settings.py
+++ SafePassword/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ifjc^jrjaf_q8y=k(($tbop9pw#j6z28#)o%j_!$#us$#ygz(%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
