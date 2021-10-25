8acf9a3dc3f47fe0cfe753f06a84706c75db61f3
Deepshikha2700 <89649896+Deepshikha2700@users.noreply.github.com>
2021-10-25 20:12:47 +0530

    Update settings.py

--- SafePassword/settings.py
+++ SafePassword/settings.py
@@ -132,8 +132,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "safecase40@gmail.com"
-EMAIL_HOST_PASSWORD = "safecase123"
+EMAIL_HOST_USER = "//add your email"
+EMAIL_HOST_PASSWORD = "//add your password"
 EMAIL_USE_TLS = True
 
 KEY=config('KEY')
